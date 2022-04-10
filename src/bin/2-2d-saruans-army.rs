be1fe70db0bfdb7b95d840e34dcab2b4a6575c3e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-04-10 18:02:29 +0900

    add 2-2d

--- src/bin/2-2d-saruans-army.rs
+++ src/bin/2-2d-saruans-army.rs
@@ -4,7 +4,21 @@ fn main() {
     input! {
         n:usize,
         r:usize,
-        x:[usize;n],
+        mut x:[usize;n],
     }
-    todo!();
+    x.sort();
+    let mut i = 0;
+    let mut ans = 0;
+    while i < n {
+        let s = x[i];
+        while i < n && x[i] <= s + r {
+            i += 1;
+        }
+        let p = x[i - 1];
+        while i < n && x[i] <= p + r {
+            i += 1;
+        }
+        ans += 1;
+    }
+    println!("{}", ans);
 }
