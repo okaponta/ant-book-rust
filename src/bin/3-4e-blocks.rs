0182db3291d30c4479ba52cc140ba23174f88821
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-16 01:04:41 +0900

    add 3-4e

---
+++ src/bin/3-4e-blocks.rs
@@ -0,0 +1,40 @@
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n:Usize1,
+    }
+    let modulo = 10007;
+    // フィボナッチの漸化式を行列で表現
+    let a = nalgebra::Matrix3::new(2, 1, 0, 2, 2, 2, 0, 1, 2);
+    let a = pow(a, n, modulo);
+    println!("{}", a.m11);
+}
+
+// 行列式のn乗
+fn pow(mut a: nalgebra::Matrix3<usize>, mut n: usize, modulo: usize) -> nalgebra::Matrix3<usize> {
+    let mut b = a.clone();
+    while 0 < n {
+        if n & 1 == 1 {
+            b = b * a;
+            rem(&mut b, modulo);
+        }
+        a = a * a;
+        rem(&mut a, modulo);
+        n >>= 1;
+    }
+    b
+}
+
+// 各項のmodをとる
+fn rem(a: &mut nalgebra::Matrix3<usize>, modulo: usize) {
+    a.m11 %= modulo;
+    a.m12 %= modulo;
+    a.m13 %= modulo;
+    a.m21 %= modulo;
+    a.m22 %= modulo;
+    a.m23 %= modulo;
+    a.m31 %= modulo;
+    a.m32 %= modulo;
+    a.m33 %= modulo;
+}
