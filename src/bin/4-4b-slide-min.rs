ec70fbde0f18c79fcad80a8173edca2c75ee4b10
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-05-04 01:38:41 +0900

    add 4-4b

---
+++ src/bin/4-4b-slide-min.rs
@@ -0,0 +1,34 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    input! {
+        n:usize,
+        k:usize,
+        a:[usize;n],
+    }
+    // インデックスを保持。
+    // i1 < i2 かつ a[i1] < a[i2]を満たすように。
+    // 先頭が対象インデックスなら削除する
+    let mut q = VecDeque::new();
+    let mut ans = vec![];
+    for i in 0..n {
+        while let Some(b) = q.pop_back() {
+            if b < a[i] {
+                q.push_back(b);
+                break;
+            }
+        }
+        q.push_back(i);
+        if k <= i + 1 {
+            let f = q.pop_front().unwrap();
+            ans.push(a[f]);
+            if f != i + 1 - k {
+                q.push_front(f);
+            }
+        }
+    }
+    println!("{}", ans.into_iter().join(" "));
+}
